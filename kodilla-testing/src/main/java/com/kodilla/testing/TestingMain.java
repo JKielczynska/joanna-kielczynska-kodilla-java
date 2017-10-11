4c40745d72c1fc8f3a2d48096b94990165082995
JKielczynska <joannapelczynska@gmail.com>
2017-10-11 14:28:07 +0200

    4.2 Testing methods in Calculator class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator addAToB = new Calculator();
+
+        int additionResult = addAToB.add(1,1);
+
+        if(additionResult == 2) {
+            System.out.println("Testing add method was successful!");
+        } else {
+            System.out.println("Testing add method failed.");
+        }
+
+        Calculator substractAfromB = new Calculator();
+
+        int substractionResult = substractAfromB.substract(1,1);
+
+        if(substractionResult == 0) {
+            System.out.println("Testing substract method was successful!");
+        } else {
+            System.out.println("Testing substract method failed.");
+        }
     }
 }
