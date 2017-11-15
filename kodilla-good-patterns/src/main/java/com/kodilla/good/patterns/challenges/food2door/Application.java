16d5a910fa82cfbbf3a68100dc9eb97289f04e49
JKielczynska <joannapelczynska@gmail.com>
2017-11-15 18:15:05 +0100

    9.4 Create Map in constructor

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/Application.java
@@ -1,16 +1,12 @@
 package com.kodilla.good.patterns.challenges.food2door;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Application {
     /** Method starts product ordering process.*/
     public static void main(final String[] args) {
         OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
         OrderRequest orderRequest = orderRequestRetriever.retrieve();
 
-        Map<String, Supplier> suppliersMap = new HashMap<>();
-        SupplyProcessor supplyProcessor = new SupplyProcessor(suppliersMap);
+        SupplyProcessor supplyProcessor = new SupplyProcessor();
         supplyProcessor.process(orderRequest);
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/SupplyProcessor.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/SupplyProcessor.java
@@ -1,13 +1,14 @@
 package com.kodilla.good.patterns.challenges.food2door;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SupplyProcessor {
     private Map<String, Supplier> suppliersMap;
 
     /**Method stores the map with the suppliers.*/
-    public SupplyProcessor(final Map<String, Supplier> suppliersMap) {
-        this.suppliersMap = suppliersMap;
+    public SupplyProcessor() {
+        this.suppliersMap = new HashMap<>();
 
         suppliersMap.put("EFS", new ExtraFoodShop());
         suppliersMap.put("GFS", new GlutenFreeShop());
@@ -16,7 +17,6 @@ public class SupplyProcessor {
 
     /**Method processes the order.*/
     public OrderDto process(final OrderRequest orderRequest) {
-
         for (Product product : orderRequest.getProducts()) {
             suppliersMap.get(product.getSupplierId()).process(product);
             System.out.println(product + " from " + product.getSupplierId());
