62fafc234289c6e03a2e715b9d61bc0d548fd3bd
JKielczynska <joannapelczynska@gmail.com>
2017-11-30 13:12:21 +0100

    module 13.2 - course

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -0,0 +1,67 @@
+package com.kodilla.hibernate.task;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name = "TASKS")
+public class Task {
+    private int id;
+    private String description;
+    private Date created;
+    private int duration;
+
+    public Task() {
+    }
+
+    public Task(final String description, final int duration) {
+        this.description = description;
+        this.created = new Date();
+        this.duration = duration;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    @NotNull
+    @Column(name = "CREATED")
+    public Date getCreated() {
+        return created;
+    }
+
+    @Column(name = "DURATION")
+    public int getDuration() {
+        return duration;
+    }
+
+    private void setId(final int id) {
+        this.id = id;
+    }
+
+    private void setDescription(final String description) {
+        this.description = description;
+    }
+
+    private void setCreated(final Date created) {
+        this.created = created;
+    }
+
+    private void setDuration(final int duration) {
+        this.duration = duration;
+    }
+}
---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -0,0 +1,15 @@
+package com.kodilla.hibernate.task.dao;
+
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import com.kodilla.hibernate.task.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Transactional
+@Repository
+public interface TaskDao extends CrudRepository<Task, Integer> {
+    List<Task> findByDuration(int duration);
+}
